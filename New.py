3ad351a5b87ed8e4fab1b919c86ce58d2fd4a034
Freddy <fredduke1983@gmail.com>
2022-05-22 19:59:39 +0300

    update field

--- New.py
+++ New.py
@@ -2,14 +2,12 @@ import random
 import sys
 import pygame
 from players import Players
+from cards import Cards
 
 def run():
     pygame.init()
-    screen = pygame.display.set_mode((800,600))
+    screen = pygame.display.set_mode((900,600))
 
-    pic = pygame.image.load("image/images.jpg")
-    pic2 = pic3 = pic4 = pygame.image.load("image/trap.png")
-    pic5 = pic6 = pic7= pic8 = pygame.image.load("image/empty.png")
     cubic1 = pygame.image.load("image/cubic_1.jpg")
     cubic2 = pygame.image.load("image/cubic_2.jpg")
     cubic3 = pygame.image.load("image/cubic_3.jpg")
@@ -22,17 +20,18 @@ def run():
     pl1 = pygame.image.load("image/lovepik.png")
     player1 = Players(pl1, "Fred", 1)
 
-    print("Player #",player1.id())
-    print("Position Player X ->", player1.positionX())
-    print("Position Player Y ->", player1.positionY())
-    field = [pic, pic2, pic3, pic4, pic5, pic6, pic7, pic8]
+    new_cards = []
+    for n in range (18):
+        card = Cards(n)
+        new_cards.append(card)
 
     pos1 = 10
     click = 1
+    mouse_click_count = 0
     random_cubic = 0
 
     FPS = 10
-    mouse_click_count = 0
+
     clock = pygame.time.Clock()
     x, y = 50, 50
     pl_x, pl_y = 30, 50
@@ -59,13 +58,11 @@ def run():
 
                     if but_x > 50 and but_x < 150 and but_y > 450 and but_y < 550:
                         random_cubic = random.randint(0, 5)
-                        print(but_x, but_y, "   ", random_cubic+1)
-
-                        # pos1 += (random_cubic+1)*100
                         if pl_x > 600:
 
                             pl_x, pl_y = player1.move(0, random_cubic+1)
-                            print("plx2 = ", pl_x)
+                            pr
+                            int("plx2 = ", pl_x)
                             print("ply2 = ", pl_y)
                         else:
 
@@ -77,31 +74,22 @@ def run():
         if click == 1:
             click = 0
             col = 1
-            for i in field:
 
-                if col < 6:
-                    screen.blit(i, (x, y))
-                    pygame.draw.rect(screen, (50, 50, 250), (x, y, 100, 100), 2)
-                    x += 100
-                    col += 1
-                else:
-                    screen.blit(i, (x, y))
-                    pygame.draw.rect(screen, (50, 50, 250), (x, y, 100, 100), 2)
-                    y += 100
+            for i in range(1, len(new_cards)):
+                x, y = new_cards[i].coordinate()
+                pic = new_cards[i].card()
+                screen.blit(pic,(x, y))
+                pygame.draw.rect(screen, (50, 200, 150), (x, y, 100, 100), 2)
+
 
             screen.blit(cubic[random_cubic], (50, 450))
             if pl_x > 600:
                 pl_x = pl_x - (random_cubic+1)*100
                 for coun in range(0, (random_cubic+1)*100):
-                    # print(coun)
                     pl_x += 2
                     if pl_x > 629:
-                        print(' Player position X --',player1.positionX(), ' pl_x = ',pl_x)
                         pl_x, pl_y = player1.move(-1, int((player1.positionX()-pl_x)/100))
-                        print(' Player position X --', player1.positionX(), ' pl_x = ', pl_x)
-                        print(' pl_y ----', pl_y)
                         break
-            print(' pl_y --', pl_y, ' pl_x = ', pl_x)
             screen.blit(pl1, (pl_x-60, pl_y))
 
             x = 50
--- cards.py
+++ cards.py
@@ -1,6 +1,12 @@
 import pygame
+import random
 
+pic1 = pygame.image.load("image/images.jpg")
+pic2 = pygame.image.load("image/trap.png")
+pic3 = pygame.image.load("image/empty.png")
+pics = [pic1, pic2, pic3]
 coord = {}
+
 
 x0 = 50
 y0 = 50
@@ -15,13 +21,15 @@ for i in range (12,20):
     x0 -= 100
 
 class Cards:
-    trap = pygame.image.load("image/trap.png")
-    pic = pygame.image.load("image/images.jpg")
-    ce = pygame.image.load("image/empty.png")
+
     def __init__(self, id):
+        self.pictures = pics[random.randint(0, 2)]
         self.id = id
     def coordinate(self):
         return coord[self.id]
+    def card(self):
+        return self.pictures
+
 
 
 
